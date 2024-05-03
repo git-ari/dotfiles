4408b8d810bf2dc97627e5c5ff1f8af3fdde2e21
Ari <ari.xxxx@protonmail.com>
2024-05-03 10:26:13 +0100

    Add fuzzy finder

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ fi
 
 # Repeated keys in VSCode _won't_ pop up the accent/umlaut menu
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# Command-line fuzzy finder
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --completion --update-rc 
